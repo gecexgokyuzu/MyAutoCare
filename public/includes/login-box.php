09c8be063d6ede04072fde4f84ad88f0ddf6687e
gecexgokyuzu <yukselhakanderin@gmail.com>
2023-06-19 11:17:20 +0200

    login box, includes içine taşındı, login.php login-box'u çağırıyor, login-boxu index'in içine çağırabiliriz.

---
+++ public/includes/login-box.php
@@ -0,0 +1,19 @@
+<div class="d-flex align-items-center">
+            <div class="login-box">
+                <form action="login.php" method="post">
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="email" placeholder="your@email.com">
+                        <label for="email">Email</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="password" placeholder="Password">
+                        <label for="password">Password</label>
+                    </div>
+                    <div class="form-group form-check mb-2 mx-1">
+                        <input type="checkbox" class="form-check-input" id="remember">
+                        <label class="form-check-label" for="remember">Keep me logged in</label>
+                    </div>
+                    <input type="submit" value="Login">
+                </form>
+            </div>
+        </div>
