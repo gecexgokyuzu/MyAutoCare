1b4e339a1aadea33ff94b59aca87a60995ff1735
gecexgokyuzu <yukselhakanderin@gmail.com>
2023-06-19 19:20:07 +0200

    added the login method selection buttons

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -1,21 +1,51 @@
 // FORM VALIDATION
 
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+// TOGGLE INPUTS
+function toggleInput(type) {
+  var inputTypes = ['email', 'phone', 'idnumber'];
+
+  for (var i = 0; i < inputTypes.length; i++) {
+      var inputWrapper = document.getElementById(inputTypes[i] + '-input-wrapper');
+      var inputElement = document.getElementById(inputTypes[i] + '-input');
+      var button = document.getElementById(inputTypes[i] + '-button');
+
+      inputWrapper.classList.remove('show');
+      inputWrapper.classList.add('d-none');
+      inputElement.classList.add('d-none');
+      button.disabled = false;
+  }
+
+  var selectedInputWrapper = document.getElementById(type + '-input-wrapper');
+  var selectedInputElement = document.getElementById(type + '-input');
+  var selectedButton = document.getElementById(type + '-button');
+
+  selectedInputWrapper.classList.remove('d-none');
+  selectedInputWrapper.classList.add('d-block');
+  selectedInputElement.classList.remove('d-none');
+
+  setTimeout(function() {
+      selectedInputWrapper.classList.add('show');
+  }, 10);
+
+  selectedButton.disabled = true;
+}
--- public/includes/login-box.php
+++ public/includes/login-box.php
@@ -1,45 +1,51 @@
 <div class="d-flex align-items-center">
-    <div class="login-box">
-        <form action="login.php" method="post">
-            <h4 class="text-light text-center mb-1">Please choose login method</h4>
-            <div class="d-flex mb-3 justify-content-around">
-                <button type="button" class="btn btn-sm btn-primary col-3" disabled>Email</button>
-                <button type="button" class="btn btn-sm btn-primary col-3">Phone</button>
-                <button type="button" class="btn btn-sm btn-primary col-3">ID Number</button>
-            </div>
-            <div class="form-floating mb-3">
+    <div class="login-box has-form">
+        <h5 class="text-light text-center mb-2 text-decoration-underline">Please choose a login method</h5>
+        <div class="d-flex mb-4 justify-content-around">
+            <button type="button" class="btn btn-sm btn-primary col-3" id="email-button" onclick="toggleInput('email')" disabled>Email</button>
+            <button type="button" class="btn btn-sm btn-primary col-3" id="phone-button" onclick="toggleInput('phone')">Phone</button>
+            <button type="button" class="btn btn-sm btn-primary col-3" id="idnumber-button" onclick="toggleInput('idnumber')">ID Number</button>
+        </div>
+        <div class="form-floating mb-3 fade slow-fade show" id="email-input-wrapper">
+            <div class="form-floating mb-3" id="email-input">
                 <input type="email" class="form-control needs-validation" id="email" placeholder="your@email.com" autocomplete="email" required>
                 <label for="email">Email</label>
                 <div class="invalid-feedback">
                     Please enter a valid email address.
                 </div>
             </div>
-            <div class="form-floating mb-3">
+        </div>
+        <div class="form-floating mb-3 fade slow-fade d-none" id="phone-input-wrapper">
+            <div class="form-floating mb-3 d-none" id="phone-input">
                 <input type="tel" class="form-control needs-validation" id="phone" placeholder="012 234 5678" autocomplete="tel" required>
                 <label for="phone">Phone</label>
                 <div class="invalid-feedback">
                     Please enter a valid phone number.
                 </div>
             </div>
-            <div class="form-floating mb-3">
+        </div>
+        <div class="form-floating mb-3 fade slow-fade d-none" id="idnumber-input-wrapper">
+            <div class="form-floating mb-3 d-none" id="idnumber-input">
                 <input type="number" class="form-control needs-validation" id="idnumber" placeholder="123456789" autocomplete="on" required>
                 <label for="idnumber">ID Number</label>
                 <div class="invalid-feedback">
                     Please enter a valid ID Number.
                 </div>
             </div>
-            <div class="form-floating">
-                <input type="password" class="form-control needs-validation" id="password" placeholder="Password" autocomplete="current-password" required>
-                <label for="password">Password</label>
-                <div class="invalid-feedback">
-                    Please enter your password.
-                </div>
+        </div>
+        <div class="form-floating">
+            <input type="password" class="form-control needs-validation" id="password" placeholder="Password" autocomplete="current-password" required>
+            <label for="password">Password</label>
+            <div class="invalid-feedback">
+                Please enter your password.
             </div>
-            <div class="form-group form-check mb-2 mx-1">
-                <input type="checkbox" class="form-check-input" id="remember">
-                <label class="form-check-label" for="remember">Keep me logged in</label>
-            </div>
-            <input type="submit" value="Login">
-        </form>
+        </div>
+        <div class="form-group form-check mb-2 mx-1">
+            <input type="checkbox" class="form-check-input" id="remember">
+            <label class="form-check-label" for="remember">Keep me logged in</label>
+        </div>
+        <div class="w-100">
+            <button class="btn btn-primary btn-action btn-block login-button w-100"> <!--<?= $lang['text']['Login'] ?>-->Login</button>
+        </div>
     </div>
 </div>
