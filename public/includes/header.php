d02e29959fa24033fc2b6cc03313b4783e9ba4ad
gecexgokyuzu <yukselhakanderin@gmail.com>
2023-06-19 15:35:14 +0200

    live chat butonu, collapsing live chat eklendi. responsive ayarlandı.

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,20 +1,20 @@
-<header class="d-flex-inline bg-dark text-light">
+<header class="sticky-top d-flex-inline bg-dark text-light">
     <div class="container">
         <nav class="navbar navbar-expand-md">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle Navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <a href="#" class="navbar-brand d-md-none">
                 <img src="../assets/images/CMHlogo.png" alt="Logo" class="navbar-logo">
             </a>
-            <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
-                <ul class="navbar-nav mr-auto float-start justify-content-between nav-icons">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mr-auto float-start">
                     <!-- Home, Support, Contact icons -->
                     <li class="nav-item">
                         <a class="nav-link" href="#"><i class="fi fi-sr-house-chimney"></i></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#"><i class="fi fi-sr-headset"></i></a>
+                        <a class="nav-link" href="#live-chat" role="button" data-bs-target="#live-chat" data-bs-toggle="collapse" aria-controls="live-chat" aria-expanded="false"><i class="fi fi-sr-headset"></i></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#"><i class="fi fi-sr-envelope"></i></a>
@@ -24,7 +24,7 @@
                     <!-- Logo -->
                     <img src="../assets/images/CMHlogo.png" alt="Logo" class="navbar-logo">
                 </div>
-                <ul class="navbar-nav ml-auto float-end justify-content-between nav-icons">
+                <ul class="navbar-nav ml-auto float-end">
                     <!-- Profile, Notifications, Message Inbox icons -->
                     <li class="nav-item">
                         <a class="nav-link" href="#"><i class="fi fi-sr-user"></i></a>
