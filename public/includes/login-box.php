4618d9cc122984a21c4de6ffcbd9a154435466c4
gecexgokyuzu <yukselhakanderin@gmail.com>
2023-06-20 09:47:14 +0200

    on input form and control labels

--- public/includes/login-box.php
+++ public/includes/login-box.php
@@ -9,8 +9,8 @@
         <div class="input-carousel mb-3">
             <div class="form-floating mb-3 fade d-none slow-fade" id="email-input-wrapper">
                 <div class="form-floating mb-3" id="email-input">
-                    <input type="email" class="form-control needs-validation" id="email" placeholder="your@email.com" autocomplete="email" required>
-                    <label for="email">Email</label>
+                    <input type="email" class="form-control needs-validation" id="email" data-name0="email" placeholder="<?= $lang["text"]["your@email.com"] ?>" autocomplete="email" required>
+                    <label for="email"><?= $lang["text"]["Email"] ?></label>
                     <div class="invalid-feedback">
                         Please enter a valid email address.
                     </div>
@@ -18,8 +18,8 @@
             </div>
             <div class="form-floating mb-3 fade d-none slow-fade" id="phone-input-wrapper">
                 <div class="form-floating mb-3" id="phone-input">
-                    <input type="tel" class="form-control needs-validation" id="phone" placeholder="012 234 5678" autocomplete="tel" required>
-                    <label for="phone">Phone</label>
+                    <input type="tel" class="form-control needs-validation" id="phone" data-name0="phone" placeholder="<?= $lang["text"]["Phone"] ?>" autocomplete="tel" required>
+                    <label for="phone"><?= $lang["text"]["Phone"] ?></label>
                     <div class="invalid-feedback">
                         Please enter a valid phone number.
                     </div>
@@ -27,8 +27,8 @@
             </div>
             <div class="form-floating mb-3 fade d-none slow-fade" id="idnumber-input-wrapper">
                 <div class="form-floating mb-3" id="idnumber-input">
-                    <input type="number" class="form-control needs-validation" id="idnumber" placeholder="123456789" autocomplete="on" required>
-                    <label for="idnumber">ID Number</label>
+                    <input type="number" class="form-control needs-validation" id="idnumber" data-name0="idnumber" placeholder="<?= $lang["text"]["ID Number"] ?>" autocomplete="on" required>
+                    <label for="idnumber"><?= $lang["text"]["ID Number"] ?></label>
                     <div class="invalid-feedback">
                         Please enter a valid ID Number.
                     </div>
@@ -36,18 +36,18 @@
             </div>
         </div>
         <div class="form-floating">
-            <input type="password" class="form-control needs-validation" id="password" placeholder="Password" autocomplete="current-password" required>
-            <label for="password">Password</label>
+            <input type="password" class="form-control needs-validation" id="password" placeholder="<? $lang["text"]["Password"] ?>" data-name0="password" autocomplete="current-password" required>
+            <label for="password"><?= $lang["text"]["Password"] ?></label>
             <div class="invalid-feedback">
                 Please enter your password.
             </div>
         </div>
         <div class="form-group form-check mb-2 mx-1">
             <input type="checkbox" class="form-check-input" id="remember">
-            <label class="form-check-label" for="remember">Keep me logged in</label>
+            <label class="form-check-label" for="remember"><?= $lang["text"]["Keep me logged in"] ?></label>
         </div>
         <div class="w-100">
-            <button class="btn btn-primary btn-action btn-block login-button w-100"> <!--<?= $lang['text']['Login'] ?>-->Login</button>
+            <button class="btn btn-primary btn-action btn-block login-button w-100"><?= $lang['text']['Login'] ?></button>
         </div>
     </div>
 </div>
--- public/login.php
+++ public/login.php
@@ -18,6 +18,7 @@
 <body>
     <?php include 'includes/header.php'; ?>
     <main class="flex-shrink-0">
+        <?php include 'includes/live-chat.php'; ?>
         <?php include 'includes/login-box.php'; ?>
     </main>
     <?php include 'includes/footer.php'; ?>
@@ -26,6 +27,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- Custom JS -->
     <script src="assets/javascript/script.js"></script>
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </body>
 
 </html>
